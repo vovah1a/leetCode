e9908468f447432336ba7551bb6292ed09483546
AVladimir <vovah1a@gmail.com>
2024-02-02 18:47:51 +0300

    problem/015.3Sum

---
+++ problems/015.3Sum/go.mod
@@ -0,0 +1,3 @@
+module github.com/vovah1a/leetCode/problems/015.3Sum
+
+go 1.21.6
---
+++ problems/015.3Sum/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums := []int{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6}
+	fmt.Println(threeSum(nums))
+}
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := [][]int{}
+
+	for i := range nums {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		l, r := i+1, len(nums)-1
+
+		for l < r {
+			s := nums[i] + nums[l] + nums[r]
+			switch {
+			case s < 0:
+				l++
+			case s > 0:
+				r--
+			default:
+				res = append(res, []int{nums[i], nums[l], nums[r]})
+				l, r = next(nums, l, r)
+			}
+		}
+	}
+
+	return res
+}
+
+func next(nums []int, l, r int) (int, int) {
+	for l < r {
+		switch {
+		case nums[l] == nums[l+1]:
+			l++
+		case nums[r] == nums[r-1]:
+			r--
+		default:
+			l++
+			r--
+			return l, r
+		}
+	}
+
+	return l, r
+}
